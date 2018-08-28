beb0b8905699ff47ef5a0397128033c29ea852f1
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-08-27 17:22:44 -0700

    media query

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,10 +3,12 @@ import "./Navbar.css"
 
 const Navbar = (props) => (
     <div className="container">
-    
-         
-        <div className="row">
             <div className="nav">
+            {/* <div className='row hamburger-row'>
+                <div className='col-sm-1'><img  className='hamburger' src="https://png.icons8.com/material-two-tone/40/000000/menu.png"/></div>
+                <div className='col-sm-11'></div>
+            </div> */}
+            <div className="row">
                 <div className="col-sm-2"><a href="#">Home</a></div>
                 <div className="col-sm-2"><a href="#">About</a></div>
                 <div className="col-sm-2"><a href="#">Listings</a></div>
