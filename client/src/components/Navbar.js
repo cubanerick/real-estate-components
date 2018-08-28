165c6f4d889a250bb26c27f98ef022713eb21450
Kyle Sisson <kylegsisson@gmail.com>
2018-08-27 17:20:25 -0700

    added onClick

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = (props) => (
          
         <div className="row">
             <div className="nav">
-                <div className="col-sm-2"><a href="#">Home</a></div>
+                <div className="col-sm-2"><a onClick={()=>console.log("helloworld")} href="#">Home</a></div>
                 <div className="col-sm-2"><a href="#">About</a></div>
                 <div className="col-sm-2"><a href="#">Listings</a></div>
                 <div className="col-sm-2"><a href="#">Properties</a></div>
