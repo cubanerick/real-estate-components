cdd94b47b4e219a5c1a91fb40b63cd93404f5f3c
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-08-27 18:07:51 -0700

    nav-toggle

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Navbar.css"
 
-const Navbar = (props) => (
-    <div className="container">
-            <div className="nav">
-            {/* <div className='row hamburger-row'>
-                <div className='col-sm-1'><img  className='hamburger' src="https://png.icons8.com/material-two-tone/40/000000/menu.png"/></div>
-                <div className='col-sm-11'></div>
-            </div> */}
-            <div className="row">
-                <div className="col-sm-2"><a href="#">Home</a></div>
-                <div className="col-sm-2"><a onClick={()=>console.log("helloworld")} href="#">Home</a></div>
-                <div className="col-sm-2"><a href="#">About</a></div>
-                <div className="col-sm-2"><a href="#">Listings</a></div>
-                <div className="col-sm-2"><a href="#">Properties</a></div>
-                <div className="col-sm-2"><a href="#">Communities</a></div>
-                <div className="col-sm-2"><a href="#">Contact</a></div> 
-            </div>
-            
-        </div>
+
+class Navbar extends Component {
+
+    state = {
+        style: "class"
+    }
+
+    toggleNav=()=>{
         
-    
-    </div>
-    
-)
+    }
+
+
+    render(props){
+        return(
+        <div className="container">
+            <div className="nav">
+                <div className='row hamburger-row' >
+                    <div className='col-sm-1'><img  className='hamburger' src="https://png.icons8.com/material-two-tone/40/000000/menu.png" onClick={this.toggleNav} /></div>
+                    <div className='col-sm-11'></div>
+                </div>
+                <div className="row nav-row" style={this.state.style}>
+                    <div className="col-sm-2 nav-link"><a onClick={()=>console.log("helloworld")} href="#">Home</a></div>
+                    <div className="col-sm-2 nav-link"><a href="#">About</a></div>
+                    <div className="col-sm-2 nav-link"><a href="#">Listings</a></div>
+                    <div className="col-sm-2 nav-link"><a href="#">Properties</a></div>
+                    <div className="col-sm-2 nav-link"><a href="#">Communities</a></div>
+                    <div className="col-sm-2 nav-link"><a href="#">Contact</a></div> 
+                </div>
+            </div>
+        </div>
+        )}   
+}
 
 export default Navbar;
